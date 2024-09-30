82ba6cc40f2208b75e4b4e1693852a07cf85f1ce
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-09-30 11:35:39 +0900

    01_01 完了

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+template <typename Type>
+Type Min(Type a, Type b) {
+	if (a > b) {
+		return b;
+	}
+	return a;
+}
+template <>
+char Min<char>(char a, char b) {
+	printf_s("数字以外は代入できません\n");
+	return 0;
+}
+
+int intA = 3;
+int intB = 9;
+
+float floatA = 3.5f;
+float floatB = 8.7f;
+
+double doubleA = 5.37;
+double doubleB = 2.917;
+
 int main() {
-	printf_s("Hellow World!\n");
-	printf_s("こんにちは!\n");
+	printf_s("%d\n", Min<int>(intA, intB));
+	printf_s("%f\n", Min<float>(floatA, floatB));
+	printf_s("%lf\n", Min<double>(doubleA, doubleB));
+	Min<char>(6, 3);
 	return 0;
 }
