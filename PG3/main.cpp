8e57cf2012c4df203559b92710dbcd5fdc06fae1
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-10-16 17:36:37 +0900

    完成

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,45 +1,71 @@
 #include <stdio.h>
+#include <functional>
 #include <Windows.h>
 #include <random>
 #include <time.h>
 
 
-typedef void (*PFunc)(int*);
-void setTimeout(int* second) {
-	Sleep(*second * 1000);
+void setTimeout(int second) {
+	Sleep(second * 1000);
 }
 
-void RandomDice(int* ans) {
-	int dice = rand() % 6;
-	if (dice % 2 == *ans) {
-		printf_s("正解 : サイコロの目%d\n\n\n", dice);
-	}
-	else {
-		printf_s("不正解 : サイコロの目%d\n\n\n", dice);
-	}
-}
-
-void DiceGame(PFunc pTime, int second, PFunc randomDice, int& ans) {
-	while (true) {
-		ans = 0;
-		printf_s("丁(偶数)か半(奇数)か\n");
-		printf_s("丁 : 0を入力\n");
-		printf_s("半 : 1を入力\n");
-		printf_s("入力 : ");
-		scanf_s("%d", &ans);
-		if (ans == 0 || ans == 1) {
-			pTime(&second);
-			randomDice(&ans);
-		}
-	}
-}
+//void RandomDice(int* ans) {
+//	int dice = rand() % 6;
+//	if (dice % 2 == *ans) {
+//		printf_s("正解 : サイコロの目%d\n\n\n", dice);
+//	}
+//	else {
+//		printf_s("不正解 : サイコロの目%d\n\n\n", dice);
+//	}
+//}
+//
+//void DiceGame(PFunc pTime, int second, PFunc randomDice, int& ans) {
+//	while (true) {
+//		ans = 0;
+//		printf_s("丁(偶数)か半(奇数)か\n");
+//		printf_s("丁 : 0を入力\n");
+//		printf_s("半 : 1を入力\n");
+//		printf_s("入力 : ");
+//		scanf_s("%d", &ans);
+//		if (ans == 0 || ans == 1) {
+//			pTime(&second);
+//			randomDice(&ans);
+//		}
+//	}
+//}
 
 int main() {
 	srand(0);
 	int ans = 0;
-	PFunc p = setTimeout;
-	PFunc random = RandomDice;
-	DiceGame(p, 3, random, ans);
+	int sleepTime = 3;
+	std::function<void(int&)> input = [&](int& ans) {scanf_s("%d", &ans); };
+	std::function<bool(int)> checkInput = [&](int ans) {
+		return (ans == 0 || ans == 1); 
+		};
+
+	std::function<void(int)> randomDice = [&](int ans) {
+		int dice = rand() % 6 + 1;
+		if (dice % 2 == ans) {
+			printf_s("正解 : サイコロの目%d\n\n\n", dice);
+		}
+		else {
+			printf_s("不正解 : サイコロの目%d\n\n\n", dice);
+		}};
+	
+	while (true) {
+		printf_s("丁(偶数)か半(奇数)か\n");
+		printf_s("丁 : 0を入力\n");
+		printf_s("半 : 1を入力\n");
+		printf_s("入力 : ");
+		printf_s("%d", ans);
+		if (checkInput(ans)) {
+			setTimeout(sleepTime);
+			randomDice(ans);
+		}
+		else{
+			printf_s("無効な入力です\n\n");
+		}
+	}
 
 	return 0;
 }
