8de8675fbff20b6f6d57bbcdd7ce62d3f546a21d
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-11-19 12:44:54 +0900

    修正

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "Circle.h"
 #include "Rectangle.h"
+#include "IShape.h"
 #include <memory>
 
 int main() {
-	std::unique_ptr<Circle> circle = std::make_unique<Circle>(4.5f);
-	std::unique_ptr<Rectangle> rect = std::make_unique<Rectangle>(3.7f, 4.2f);
+	std::unique_ptr<IShape> circle = std::make_unique<Circle>(4.5f);
+	std::unique_ptr<IShape> rect = std::make_unique<Rectangle>(3.7f, 4.2f);
 
 	circle->Size();
 	rect->Size();
